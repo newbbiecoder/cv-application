41efc99412e9af97f317a2bd4e2d436316a37d50
Rudraksh <rudrakshkumar2908@gmail.com>
2025-10-21 18:58:25 +0000

    Edited addFields section to include/exclude styles when the button collapse in/collapse-out Added styling for job info div and add-position & close-position button Added event to handle close-position so that form field deletes after user clicks on it

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,6 +6,7 @@ export default function ExperienceInput() {
     const addForm = () => {
         setForms([...forms, <JobInfo key={crypto.randomUUID()} />])
     }
+    
 
     return (
         <div className="experience">
@@ -15,38 +16,64 @@ export default function ExperienceInput() {
             <p>a few seconds to your CV, so fill in the job</p>
             <p>description as briefly as possible. Ideally in points.</p>
 
-            <button className="add-position" onClick={addForm}>
-                Add a position
-                {/* <img src="" alt="" /> */}
-            </button>
+            <div className="btn-container">
+                {forms}
 
-            {forms}
+                <button className="add-position" onClick={addForm}>
+                    <p>Add a position</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+                </button>
 
-            <button className="btn-education">
-                Continue to education 
-                {/* <img src="" alt="" /> */}
-            </button>
+                <button className="btn-education">
+                    <p>Continue to education</p>
+                    {/* <img src="" alt="" /> */}
+                </button>
+            </div>           
         </div>
     )
 }
 
 function JobInfo() {
+    const [fieldsHide, setFieldsHide] = useState(false);
+    
     const formFields = [
-        {name: "Position*", id: crypto.randomUUID()},
-        {name: "Company*", id: crypto.randomUUID()},
-        {name: "Locality*", id: crypto.randomUUID()}
+        {name: "Position", id: crypto.randomUUID()},
+        {name: "Company", id: crypto.randomUUID()},
+        {name: "Locality", id: crypto.randomUUID()}
     ]
+
+    const addFields = (e) => {
+        setFieldsHide(!fieldsHide);
+        console.log(e.target);
+        !fieldsHide ? (
+            (e.target.tagName.toLowerCase() === 'p' || e.target.tagName.toLowerCase() === 'svg') ? e.target.parentNode.parentNode.classList.add('hide')
+            : (e.target.tagName.toLowerCase() === 'path') ? e.target.parentNode.parentNode.parentNode.classList.add('hide')
+            : e.target.parentNode.classList.add('hide')
+        ) : e.target.parentNode.classList.remove('hide');
+    }
+
+    const removeForm = (e) => {
+        e.target.parentElement.parentElement.parentElement.remove();
+    }
 
     return (
         <div className="job-info">
-            <button>
-                Company, Position
-                {/* <img src="" alt="" /> */}
+            <button onClick={addFields}>
+                <p>Company, Position</p>
+
+                {fieldsHide ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
+                ): (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
+                )}
             </button>
-            <div className="form-fields">
+            <div className={fieldsHide ? "form-fields hidden" : "form-fields"}>
                 {formFields.map(field => (
                     <label htmlFor={field.name} key={field.id}>
-                        {field.name} {' '}
+                        <p>
+                            {field.name} {' '}
+                            <span>*</span>
+                        </p>
                         <input type="text" name={field.name} id={field.name} />
                     </label>
                 ))}
@@ -85,6 +112,11 @@ function JobInfo() {
                     Reason for termination
                     <textarea name="reason-term" id="reason-term"></textarea>
                 </label>
+
+                <button className="remove-position" onClick={removeForm}>
+                    <p>Remove this position</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
+                </button>
             </div>
         </div>
     )
