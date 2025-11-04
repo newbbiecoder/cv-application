4c1901a9a4c14d1e2424ca583f4909ea48e2b62a
Rudraksh <rudrakshkumar2908@gmail.com>
2025-11-04 14:02:00 +0000

    Changed opacity 0 from input label in addPhoto section to display none to fix height issue of addPhoto svg icon

--- src/components/personalData.jsx
+++ src/components/personalData.jsx
@@ -60,7 +60,7 @@ export default function PersonalDataInput(props) {
                     <div>
                         <p>Upload photo</p>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>
-                        <input type="file" name="uploadPhoto" id="uploadPhoto" accept="image/*" onChange={handleFileChange} style={{opacity: 0}} />
+                        <input type="file" name="uploadPhoto" id="uploadPhoto" accept="image/*" onChange={handleFileChange} style={{display: 'none'}} />
                     </div>    
                 </label>
             ) : (
